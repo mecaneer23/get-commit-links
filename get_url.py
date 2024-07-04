c14167ac1b330b03220dfff8f359e1aee600ab97
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2024-07-04 16:41:25 -0500

    fix: format and satisfy pylint

--- get_url.py
+++ get_url.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
+"""
+Fetch public links to most recent git commits locally
+"""
 
 from subprocess import run
 from sys import argv
 
 
 def main(count: int) -> None:
+    """Entry point for get_url"""
     remote_url = (
-        run("git remote get-url origin".split(), capture_output=True)
+        run(
+            "git remote get-url origin".split(),
+            capture_output=True,
+            check=True,
+        )
         .stdout.decode()
         .rstrip()
         .removesuffix(".git")
     )
-    git_log = run(f"git log -n {count}".split(), capture_output=True).stdout.decode()
+    git_log = run(
+        f"git log -n {count}".split(),
+        capture_output=True,
+        check=True,
+    ).stdout.decode()
     shas = filter(lambda x: x.startswith("commit "), git_log.split("\n"))
     for sha in shas:
         print(f"{remote_url}/commit/{sha.split()[1]}\n")
