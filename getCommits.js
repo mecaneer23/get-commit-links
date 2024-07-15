ee0809c071240123af43c7d90ae9f7c8d4d1940a
mecaneer23 <mecaneer23@gmail.com>
2024-07-14 19:51:23 -0500

    feat: add autofill most recent repository button

--- getCommits.js
+++ getCommits.js
@@ -46,6 +46,43 @@ function copy() {
     navigator.clipboard.writeText(textarea.value);
 }
 
+async function getMostRecentRepo(user) {
+    const url = `https://api.github.com/users/${user}/events/public`;
+    return fetch(url)
+        .then(async response => {
+            data = await response.json();
+            if (!response.ok || data.length === 0) {
+                return [{ name: "repo_fetch_failed" }];
+            }
+            return data;
+        })
+        .then(data => {
+            const mostRecentPushEvent = data.find(event => event.type === 'PushEvent');
+            if (!mostRecentPushEvent) {
+                console.log('No push events found.');
+                return;
+            }
+            return mostRecentPushEvent.repo.name.split("/")[1];
+        })
+        .catch(error => {
+            console.error('Error fetching events:', error);
+        });
+}
+
+async function autoFillRecentRepo() {
+    const user = document.getElementById("user").value;
+    if (!user) {
+        alert("No user specified");
+        return;
+    }
+    const repo = await getMostRecentRepo(user);
+    if (!repo) {
+        alert("No repositories found for specified user");
+        return;
+    }
+    document.getElementById("repo").value = repo;
+}
+
 const textarea = document.querySelector("textarea");
 const countLabel = document.getElementById("count-label");
 
